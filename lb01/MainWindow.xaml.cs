9125d6d1b9a3765ffadc40dd08aa8eacbd3cb29c
Osyo <819705364@qq.com>
2017-05-14 22:07:00 +0800

    作业初始化

---
+++ lb01/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace lb01
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 文件读取
+        private void readData(object sender, RoutedEventArgs e)
+        {
+            ReadFile readFile = new ReadFile();
+            List<string> pointList = readFile.readFile();
+            pointList.ForEach(print);
+        }
+        #endregion
+
+        private void print (string p)
+        {
+            Console.WriteLine(p);
+        }
+
+        #region 高程计算
+        private void elevation(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 水准网平差
+        private void levellingNetwork(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 精度计算
+        private void precition(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 保存数据
+        private void saveData(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 退出
+        private void closeWindow(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ lb01/ReadFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb01
+{
+    class ReadFile
+    {
+        public List<string> readFile()
+        {
+            List<string> lines = new List<string>();
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            f.RestoreDirectory = true;
+
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader r = new StreamReader(f.OpenFile()))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+
+            return lines;
+        }
+
+        private void handleLine (string line)
+        {
+
+        }
+    }
+}
